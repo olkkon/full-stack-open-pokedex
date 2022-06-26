70ad88c93af3c238a39b6c3ae60b5b3bf2579e1e
Konsta <konstaduck@gmail.com>
2022-06-26 17:41:12 +0300

    11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,11 +1,11 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000/pokemon/doduo')
+    cy.contains('doduo').click()
     cy.contains('doduo')
     cy.contains('run away')
     cy.contains('tangled feet')
